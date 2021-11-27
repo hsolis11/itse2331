24e624e9e4785ff6c06a215c0fd2e4f6c227acc4
hector solis <hsolis11@outlook.com>
2021-11-26 21:13:35 -0600

    adding searchSort base templating

--- program14/main.cpp
+++ program14/main.cpp
@@ -6,11 +6,19 @@
 
 void instructions() {
     cout << "Enter one of the following:\n"
-         << " 1 to insert at begining of list\n"
-         << " 2 to insert at end of list\n"
-         << " 3 to delete from begining of list\n"
-         << " 4 to delete from end of list\n"
-         << " 5 to end list processing\n";
+         << " 1. to insert at begining of list\n"
+         << " 2. to insert at end of list\n"
+         << " 3. to delete from begining of list\n"
+         << " 4. to delete from end of list\n"
+         << " 5. to end list processing\n";
+}
+
+void search_sort_instructions(){
+    cout << "Enter one of the following:\n"
+        << "1. linear search\n"
+        << "2. selection sort\n"
+        << "3. merge sort\n"
+        << "4. binary search\n";
 }
 
 
@@ -59,14 +67,70 @@ void testList(List<T>& listObject, const string& typeName){
     cout << "End list test\n\n";
 }
 
+template<typename T>
+void searchSortList(List<T>& listObject, const string& typeName){
+    cout << "Searching/sorting a List of " << typeName << " values\n";
+    search_sort_instructions();
+
+    int choice;
+    T value;
+
+    do {
+        cout << "? ";
+        cin >> choice;
+
+        switch(choice){
+            case 1: // TODO: linearSearch
+                cout << "Enter " << typeName << " to search in the list: ";
+                cout << "Do line search" << endl;
+//                cin >> value;
+//                int element{linearSearch(listObject, searchKey)};
+                break;
+
+            case 2: // TODO: SelectionSort
+                cout << "Enter " << typeName << ": ";
+                cout << "Do Select Sort" << endl;
+//                cin >> value;
+//                listObject.insertAtBack(value);
+//                listObject.print();
+                break;
+            case 3: // TODO: merge  Sort
+                cout << "Do merge sort" << endl;
+//                if(listObject.removeFromFront(value)){
+//                    cout << value << " removed from list\n";
+//                }
+//
+//                listObject.print();
+                break;
+            case 4: // TODO: binary search
+                cout << "Do binary search" << endl;
+//                if(listObject.removeFromBack(value)){
+//                    cout << value << " removed from list\n";
+//                }
+//
+//                listObject.print();
+                break;
+        }
+    } while (choice < 5);
+    cout << "End list test\n\n";
+}
+
+
+
 
 int main(){
 
     List<int> integerList;
     testList(integerList, "integer");
+    searchSortList(integerList, "integer");
+    // TODO: User will input the search key
+    // int element{linearSearch(integerList, searchKey)};
+    // if(element != -1) {
+    // cout
 
     List<double> doubleList;
     testList(doubleList, "double");
+    searchSortList(doubleList, "double");
 
 
     return 0;
